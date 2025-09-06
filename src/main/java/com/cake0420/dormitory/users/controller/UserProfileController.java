8bcf6196c6a4885fa1deb31528d9105d2c30885a
soowan <nick6767676767@gmail.com>
2025-09-06 18:39:22 +0900

    refactor: 가드 클로즈(guard clause) 패턴으로 개선

--- src/main/java/com/cake0420/dormitory/users/controller/UserProfileController.java
+++ src/main/java/com/cake0420/dormitory/users/controller/UserProfileController.java
@@ -33,15 +33,12 @@ public class UserProfileController {
     public ResponseEntity<String> registerUserProfile(@RequestHeader(name = "x-supabase-signature", required = false) String signatureHeader,
                                                       @RequestHeader(name = "Authorization", required = false) String authorizationHeader,
                                                       @RequestBody String payload) {
-        boolean validAuth = supabaseService.validateServiceKey(authorizationHeader);
-        boolean isValid = WebhookUtils.verifySignature(signatureHeader, payload, webClientConfig.getSupabaseWebhookSecret());
-
-        if (!validAuth) {
+        if (!supabaseService.validateServiceKey(authorizationHeader)) {
             log.warn("유효하지 않은 토큰");
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않은 토큰입니다.");
-
         }
-        if (!isValid) {
+
+        if (!WebhookUtils.verifySignature(signatureHeader, payload, webClientConfig.getSupabaseWebhookSecret())) {
             log.warn("유효하지 않은 시그니처");
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않은 시그니처입니다.");
         }
